fee8174b3bf1863ab888d66504ec3b55260411ed
Deepesh Kumar <deepesh.kumar@globalenglish.com>
2019-01-25 19:30:05 +0530

    clearing dev branch

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('./config')
-
-mongoose.connect(config.DB_CONNSTR, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log('DB Connected'), (err) => console.log(err));
-
-
--- model/index.js
+++
@@ -1,7 +0,0 @@
-const orgToLearningSpecialist = require('./orgIdToLearningSpecialist');
-const learningSpecialist = require('./learningSpecialist');
-
-module.exports = {
-    learningSpecialist,
-    orgToLearningSpecialist
-}
